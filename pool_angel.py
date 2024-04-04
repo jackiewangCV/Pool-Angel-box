b6909eb8fdab93aa8b6d929fe05df764eb162323
TriLD <triledinh102097@gmail.com>
2024-04-04 22:51:31 +0700

    update main code integaret new model

--- pool_angel.py
+++ pool_angel.py
@@ -1,66 +1,53 @@
 import cv2
-from sources import Source
-from object_detection import ObjectDetection
-from pose_detection import PoseDetection
-from object_tracking import ObjectTracking
+from sources import (Source, BenchMark)
 from pool_detection import get_pool
 import numpy as np
-import time
-import utils
-import cv2 as cv
 from datetime import datetime
 
-DETECT_EVERY_N_FRAMES=0
+import os
+from new_code.utils.trt_pose import Pose_TRT
+from property import Person
 
 class PoolAngel:
-    def __init__(self, source, detector='nanodet', tracker=None, posenet='pose_detector', visualize=False, save=False, server_url=None) -> None:
-        self.source=Source(source)
-        self.det_name=detector
-        self.detector=ObjectDetection(detector, None)
-        self.pose_detection=PoseDetection(posenet)
-        self.bechmark=utils.BenchMark()
-        self.visualize=visualize
-        self.pool_contour=None
-        self.video=None
-        self.save=save
-        self.url=server_url
-        self.tracker=None
-        if tracker is not None:
-            self.tracker=ObjectTracking(tracker=tracker)
+    def __init__(self, source, server_url=None) -> None:
+        self.source = Source(source)
+
+        self.pose_model = Pose_TRT("new_code/models/yolov8n-pose.onnx.engine")
+        self.bechmark = BenchMark()
+        self.pool_contour = None
+        self.video = None
+        self.url = server_url
         
-
-        if self.tracker is not None:
-            self.track_frame=DETECT_EVERY_N_FRAMES
-        else:
-            self.track_frame=0
-
+        output_dir = "./data/output"
+        os.makedirs(output_dir, exist_ok=True)
+        output_video = f"{output_dir}/output.mkv"
+        out_width = 640
+        out_height = 480
+        input_fps = 25
+        self.video_writer = cv2.VideoWriter(output_video, 
+                cv2.VideoWriter_fourcc(*"MJPG"), input_fps, (out_width, out_height))
 
     def run(self):
-        
-        if not self.save:
-            cv.namedWindow('Demo', cv2.WINDOW_NORMAL)
                     
         self.bechmark.start("Whole process")
-        last_img_store=datetime(2013,12,30,23,59,59)
+        last_img_store = datetime(2013,12,30,23,59,59)
 
-        while(cv.waitKey(1) < 0):
+        while True:
             self.bechmark.start("Frame procesisng")
 
             ### Frame capture
             self.bechmark.start("Frame Capture")
-            has_frame, frame=self.source.get_frame()
+            has_frame, frame = self.source.get_frame()
             self.bechmark.stop("Frame Capture")
 
             if not has_frame:
-                print('End of video')
+                print("End of video")
                 if self.video is not None:
                     self.video.release()
                 break
             
-
-            
             if self.pool_contour is None:
-                print('Doing pool detection for first time..')
+                print("Doing pool detection for first time..")
                 self.bechmark.start("mask detection")
                 self.mask,pool_contour, pool_contour_outer=get_pool(frame, self.url)
                 self.pool_contour=pool_contour[:,0,:]
@@ -68,91 +55,38 @@ class PoolAngel:
                 print(self.mask.shape)
                 self.bechmark.stop("mask detection")
 
-            self.track_frame=self.track_frame+1
-
-            if self.track_frame>DETECT_EVERY_N_FRAMES:
-                self.track_frame=0
-                ### Object detection 
-                self.bechmark.start("Object detection")
-                persons=self.detector.detect(frame)
-
-                for i, per in enumerate(persons):
-                    persons[i].update_distance(self.pool_contour, self.pool_contour_outer)
-
-                self.bechmark.stop("Object detection")
-
-                ## If object is detected
-                if len(persons)>0:
-                    self.bechmark.start("Pose detection")
-                    ## Do pose detection 
-                    persons=self.pose_detection.detect(frame, persons)
-                    print("Num person:", len(persons))
-                    self.bechmark.stop("Pose detection")
-                    
-            else:
-                if len(persons)==0:
-                    self.track_frame=DETECT_EVERY_N_FRAMES
+            ### Object detection 
+            self.bechmark.start("Object detection")
+            bboxes, scores, kpts = self.pose_model.predict(frame)
+            persons=[]
+            for i, bb in enumerate(bboxes):
+                rat = self.pose_model.compute_rat(kpts[i])
+                ps = Person(i, bbox=bboxes[i], pose=kpts[i], confidence=scores[i], ratio=rat)
+                ps.child_or_adult()
+                ps.detect_slip()
                 
-                self.bechmark.start("Object tracking")
-                ## object tracking...! 
-                if self.tracker is not None:
-                    for i, per in enumerate(persons):
-                        bbox=per.bbox[-1]
-                        bbox=[round(bbox[0]),round(bbox[1]),round(bbox[2]-bbox[0]),round(bbox[3]-bbox[1])]
-                        print(bbox)
-                        try:
-                            self.tracker.init(last_frame, bbox)
-                            is_located, bbox, confidence=self.tracker.track(frame)
-                            if is_located and confidence>0.6:
-                                bbox=[bbox[0],bbox[1],bbox[2]+bbox[0],bbox[3]+bbox[1]]
-                                persons[i].update_infos(bbox=bbox)
-                            else:
-                                print('Object is gone')
-                                self.track_frame=DETECT_EVERY_N_FRAMES
-                        except Exception as e:
-                            print(e)
-                            print(last_frame, bbox)
-                self.bechmark.stop("Object tracking")
-
-            last_frame=frame
-
-            if self.visualize:
-                ## visualize detections
-                self.bechmark.start("Visualization")
-                cv2.drawContours(frame, pool_contour[:,np.newaxis,:], -1, (0, 0, 255), 3)
-                cv2.drawContours(frame, pool_contour_outer[:,np.newaxis,:], -1, (30,255, 255), 3)
-
-                if self.det_name=='yolox':
-                    scale=(139, 0, 361, 640)
-                elif self.det_name=='yolov8':
-                    scale=None
-                else:
-                    scale=(90, 0, 235, 416)
-
-                img = utils.vis(persons, frame, scale)  ## Last argment is hardcoded
-                if not self.save:
-                    cv.imshow('Demo', img)
-                    # cv2.waitKey(0)
-                else:
-                    if self.video is None:
-                        im_size=frame.shape[:2]
-                        self.video= cv2.VideoWriter(self.save, cv2.VideoWriter_fourcc('F','M','P','4'), 30, (im_size[1],im_size[0]))
-
-                    print(img.shape,im_size) 
-                    self.video.write(img) 
+                ps.update_distance(self.pool_contour, self.pool_contour_outer)
+                persons.append(ps)
+                
+            self.bechmark.stop("Object detection")
 
 
-                self.bechmark.stop("Visualization")
+            ## visualize detections
+            self.bechmark.start("Visualization")
+            cv2.drawContours(frame, pool_contour[:,np.newaxis,:], -1, (0, 0, 255), 3)
+            cv2.drawContours(frame, pool_contour_outer[:,np.newaxis,:], -1, (30,255, 255), 3)
+            vis_img = self.pose_model.vis_pose(frame, bboxes, scores, kpts)  ## Last argment is hardcoded
+            self.video_writer.write(vis_img)
+            self.bechmark.stop("Visualization")
 
             time_now=datetime.now()
-            # print((time_now-last_img_store).total_seconds())
             if (time_now-last_img_store).total_seconds()>100:  ## store only if last event has happened in last 10min
                 ## recording
                 for per in persons:
-                    if per.dist_pool==0 or per.warn or per.is_slipped:
-                        print('saving image')
-                        last_img_store=time_now
-                        cv2.imwrite('./data/'+str(time_now)+'.png',frame)
+                    if per.dist_pool == 0 or per.warn or per.is_slipped:
+                        print("saving image")
+                        last_img_store = time_now
+                        cv2.imwrite("./data/"+str(time_now)+".png", vis_img)
                         break
             
             self.bechmark.stop("Frame procesisng")
